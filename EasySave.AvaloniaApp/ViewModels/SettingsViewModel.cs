caa6de449fb416e602508814613da91f7c65d19d
yyyanis <yanis.walid8@gmail.com>
2026-02-16 14:29:57 +0100

    Fix: adapt SettingsViewModel to existing Configuration API

---
+++ EasySave.AvaloniaApp/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using EasySave.AvaloniaApp.Helpers;
+using EasySave.Models;
+
+namespace EasySave.AvaloniaApp.ViewModels;
+
+public partial class SettingsViewModel : ViewModelBase
+{
+    private readonly Configuration _configuration;
+
+    public LocalizationHelper Loc => LocalizationHelper.Instance;
+
+    [ObservableProperty]
+    private string _cryptoSoftPath = string.Empty;
+
+    [ObservableProperty]
+    private string _extensionsText = string.Empty;
+
+    [ObservableProperty]
+    private string _businessSoftwareName = string.Empty;
+
+    [ObservableProperty]
+    private int _selectedLogFormatIndex;
+
+    [ObservableProperty]
+    private string _statusMessage = string.Empty;
+
+    public SettingsViewModel(Configuration configuration)
+    {
+        _configuration = configuration;
+        LoadFromConfig();
+    }
+
+    private void LoadFromConfig()
+    {
+        CryptoSoftPath = _configuration.CryptoSoftPath;
+        ExtensionsText = string.Join(", ", _configuration.GetEncryptExtensions());
+        BusinessSoftwareName = _configuration.GetBusinessSoftwareName();
+        SelectedLogFormatIndex = _configuration.GetLogFormat() == "xml" ? 1 : 0;
+    }
+
+    [RelayCommand]
+    private void Save()
+    {
+        _configuration.CryptoSoftPath = CryptoSoftPath;
+
+        var extensions = ExtensionsText
+            .Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+            .ToList();
+        _configuration.SetEncryptExtensions(extensions);
+
+        _configuration.SetBusinessSoftwareName(BusinessSoftwareName);
+        _configuration.SetLogFormat(SelectedLogFormatIndex == 1 ? "xml" : "json");
+
+        _configuration.SaveConfig();
+        StatusMessage = Loc["SettingsSaved"];
+    }
+}
